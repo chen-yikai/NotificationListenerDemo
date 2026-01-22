ae24aa3815a48fdae2c9c5ecc1100b124f5e211c
elias <eliaschen.dev@gmail.com>
2026-01-22 11:26:23 +0800

    feat: notification label

--- app/src/main/java/dev/eliaschen/notificationlistener/App.kt
+++ app/src/main/java/dev/eliaschen/notificationlistener/App.kt
@@ -1,18 +1,26 @@
 package dev.eliaschen.notificationlistener
 
 import android.app.Application
-import android.content.res.Configuration
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
-const val notification_channel = "notification_label_channel"
+const val notification_label_channel = "notification_label_channel"
 
 @HiltAndroidApp
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
     }
 
     private fun createNotificationChannel() {
-        
+        val manager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(
+            notification_label_channel, "Notification Label Channel",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        manager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/dev/eliaschen/notificationlistener/service/NotificationListener.kt
+++ app/src/main/java/dev/eliaschen/notificationlistener/service/NotificationListener.kt
@@ -1,17 +1,21 @@
 package dev.eliaschen.notificationlistener.service
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import androidx.collection.LruCache
+import androidx.core.app.NotificationCompat
 import dagger.hilt.android.AndroidEntryPoint
+import dev.eliaschen.notificationlistener.notification_label_channel
 import dev.eliaschen.notificationlistener.room.NotificationDao
 import dev.eliaschen.notificationlistener.room.NotificationEntity
 import dev.eliaschen.notificationlistener.util.drawableToBitmap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,7 +26,19 @@ class NotificationListener : NotificationListenerService() {
 
     @Inject
     lateinit var cache: LruCache<Long, PendingIntent>
+
+    @Inject
+    lateinit var notifyManager: NotificationManager
     private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var notificationCount = 0
+
+    companion object {
+        var notificationCount = 0
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
@@ -49,5 +65,19 @@ class NotificationListener : NotificationListenerService() {
                 cancelNotification(sbn.key)
             }
         }
+    }
+
+    private fun displayStatusNotification() {
+        serviceScope.launch {
+            notificationCount = notificationDao.getAllNotifications().count()
+        }
+        val builder = NotificationCompat.Builder(this, notification_label_channel)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Active Notifications")
+            .setContentText("You have $notificationCount active notifications")
+            .setOngoing(true)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setOnlyAlertOnce(true).build()
+        notifyManager.notify(1001, builder)
     }
 }
