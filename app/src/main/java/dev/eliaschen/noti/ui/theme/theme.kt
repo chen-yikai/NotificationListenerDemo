c684d9f8e15bf56ca0860bb9617f910722173c4b
elias <eliaschen.dev@gmail.com>
2026-01-26 14:12:13 +0800

    rollback: remove custom font

--- app/src/main/java/dev/eliaschen/noti/ui/theme/theme.kt
+++ app/src/main/java/dev/eliaschen/noti/ui/theme/theme.kt
@@ -15,30 +15,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import dev.eliaschen.noti.R
 
-val notoSerif = FontFamily(
-    Font(resId = R.font.noto_serif, weight = FontWeight.Light),
-    Font(resId = R.font.noto_serif, weight = FontWeight.Normal),
-    Font(resId = R.font.noto_serif, weight = FontWeight.Bold),
-)
-val defaultTypography = Typography()
-val Typography = Typography(
-    displayLarge = defaultTypography.displayLarge.copy(fontFamily = notoSerif),
-    displayMedium = defaultTypography.displayMedium.copy(fontFamily = notoSerif),
-    displaySmall = defaultTypography.displaySmall.copy(fontFamily = notoSerif),
-
-    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = notoSerif),
-    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = notoSerif),
-    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = notoSerif),
-
-    titleLarge = defaultTypography.titleLarge.copy(fontFamily = notoSerif),
-    titleMedium = defaultTypography.titleMedium.copy(fontFamily = notoSerif),
-    titleSmall = defaultTypography.titleSmall.copy(fontFamily = notoSerif),
-
-    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = notoSerif),
-    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = notoSerif),
-    bodySmall = defaultTypography.bodySmall.copy(fontFamily = notoSerif),
-)
-
 @Composable
 fun NotificationListenerDemoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -49,13 +25,13 @@ fun NotificationListenerDemoTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> darkColorScheme()
         else -> lightColorScheme()
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-//        typography = Typography,
         content = content
     )
 }
